5fdd6820eecaebf5f32d07767284a7ecbc185af6
visualstudio <visualstudio@122-PC01>
2019-01-18 20:44:36 -0500

    my third code

--- C0725642/Program.cs
+++ C0725642/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0725642
+namespace C0727172_Harmanjot_Singh
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Marry", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 3.8, 20);
+            StudentInformation _Mark = new StudentInformation("Mark", 3.8, 20);
+            StudentInformation _Steve = new StudentInformation("Steve", 3.8, 20);
+            StudentInformation _Cindy = new StudentInformation("Cindy", 3.8, 20);
+        }
+    }
+    class StudentInformation
+    {
+        string Name;
+        string StudentNumber;
+        double GPA;
+        int Attendance;
+        public StudentInformation(string aName, double cGPA, int dAttendance)
+        {
+            Name = aName;
+            StudentNumber = bStudentNumber;
+            GPA = cGPA;
+            Attendance = dAttendance;
         }
     }
 }
