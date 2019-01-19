416ceac33d6597e2f3bf35d4579a0fec88925318
visualstudio <visualstudio@122-PC01>
2019-01-18 20:53:32 -0500

    my 4 code

--- C0725642/Program.cs
+++ C0725642/Program.cs
@@ -20,13 +20,13 @@ namespace C0725642_Ashwinder Singh
     class StudentInformation
     {
         string Name;
-        string StudentNumber;
+        
         double GPA;
         int Attendance;
         public StudentInformation(string aName, double cGPA, int dAttendance)
         {
             Name = aName;
-            StudentNumber = bStudentNumber;
+            
             GPA = cGPA;
             Attendance = dAttendance;
         }
